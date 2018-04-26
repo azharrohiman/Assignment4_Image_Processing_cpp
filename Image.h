66f11c1a14c4f26a36309ff4c7a3a45b85324488
Azhar Rohiman <rhmmuh005@myuct.ac.za>
2018-04-26 20:35:23 +0200

    Image.h declarations added, iterator class and its operators' declarations added

--- Image.h
+++ Image.h
@@ -34,5 +34,42 @@ namespace RHMMUH005 {
 
 			void loadImage(std::string fileName);
 			void saveImage(std::string fileName);
+
+			class iterator {
+
+				friend class Image;
+
+				private:
+					unsigned char *ptr;
+					iterator(unsigned char *p) : ptr(p) {
+
+					}
+
+				public:
+					iterator(const iterator& rhs) : ptr(rhs.ptr) {
+
+					}
+
+					const iterator& operator++();
+
+					const iterator& operator--();
+
+					unsigned char operator*();
+
+					iterator& operator=(const iterator& rhs);
+
+					iterator& operator=(iterator&& rhs);
+
+					bool operator==(const iterator& rhs);
+					bool operator!=(const iterator& rhs);
+			};
+
+			iterator begin(void) const;
+			iterator end() const;
 	};
+
+	std::ostream& operator<<(std::ostream& os, const Image& image);
+	std::istream& operator>>(std::istream& is, Image& image);
 }
+
+#endif
