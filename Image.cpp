262ce5564321f47d20bb888c064e38dcdf8ac1b6
Azhar Rohiman <rhmmuh005@myuct.ac.za>
2018-04-27 13:30:34 +0200

    Operators for Image class added

--- Image.cpp
+++ Image.cpp
@@ -26,7 +26,7 @@ namespace RHMMUH005
 		height = rhs.height;
 		imgName = rhs.imgName;
 
-		data = unique_ptr<u_char[]>(new u_char[rhs.height * rhs.width]);
+		data = unique_ptr<unsigned char[]>(new unsigned char[rhs.height * rhs.width]);
 
 		for(Image::iterator beg = begin(), inStart = rhs.begin(); inStart != rhs.end(); ++beg, ++inStart)
 		{
@@ -40,7 +40,7 @@ namespace RHMMUH005
 		height = rhs.height;
 		imgName = rhs.imgName;
 
-		data = unique_ptr<u_char[]>(new u_char[rhs.height * rhs.width]);
+		data = unique_ptr<unsigned char[]>(new unsigned char[rhs.height * rhs.width]);
 
 		for(Image::iterator beg = begin() , inStart = rhs.begin(); inStart != rhs.end(); ++beg, ++inStart)
 		{
@@ -56,7 +56,7 @@ namespace RHMMUH005
 		height = rhs.height;
 		imgName = rhs.imgName;
 
-		data = unique_ptr<u_char[]>(new u_char[rhs.height * rhs.width]);
+		data = unique_ptr<unsigned char[]>(new unsigned char[rhs.height * rhs.width]);
 
 		for(Image::iterator beg = begin() , inStart = rhs.begin(); inStart != rhs.end(); ++beg, ++inStart)
 		{
@@ -75,7 +75,7 @@ namespace RHMMUH005
 		height = rhs.height;
 		imgName = rhs.imgName;
 
-		data = unique_ptr<u_char[]>(new u_char[rhs.height * rhs.width]);
+		data = unique_ptr<unsigned char[]>(new unsigned char[rhs.height * rhs.width]);
 
 		for(Image::iterator inStart = rhs.begin(), beg = begin(); inStart != rhs.end(); ++inStart, ++beg)
 		{
@@ -92,6 +92,122 @@ namespace RHMMUH005
 
 	Image::~Image() {
 
+	}
+
+	Image Image::operator+(const Image& rhs) const
+	{
+		if(height == rhs.height && width == rhs.width)
+		{
+			Image tmp(*this);
+			for(Image::iterator inStart = rhs.begin(), beg = tmp.begin(); inStart != rhs.end(); ++beg, ++inStart)
+			{
+				int number = (int)(*beg) + (int)(*inStart);
+				if (number > 255) {
+					number = 255;
+				}
+				else if (number < 0){
+					number = 0;
+				}
+				*beg = (unsigned char)(number);
+			}
+
+			return tmp;
+		}
+		else {
+			exit(1);
+		}
+	}
+
+	Image Image::operator-(const Image& rhs) const
+	{
+		if(height == rhs.height && width == rhs.width)
+		{
+			Image tmp(*this);
+			for(Image::iterator beg = tmp.begin(), inStart = rhs.begin(); inStart != rhs.end(); ++beg, ++inStart)
+			{
+				int number = (int)(*beg) - (int)(*inStart);
+				if (number > 255) {
+					number = 255;
+				}
+				else if (number < 0) {
+					number = 0;
+				}
+
+				*beg = (unsigned char)(number);
+			}
+
+			return tmp;
+		}
+		else {
+			exit(1);
+		}
+	}
+
+	Image Image::operator!() const
+	{
+		Image tmp(*this);
+		for(Image::iterator beg = begin(), inStart = tmp.begin(); beg != end(); ++beg, ++inStart)
+		{
+			int number = 255 - (int)(*beg);
+
+			if (number > 255) {
+				number = 255;
+			}
+			else if (number < 0) {
+				number = 0;
+			}
+
+			*inStart = (unsigned char)(number);
+		}
+
+		return tmp;
+	}
+
+	Image Image::operator/(const Image& rhs) const
+	{
+		if(height == rhs.height && width == rhs.width)
+		{
+			Image tmp(rhs);
+			for(Image::iterator inStart = tmp.begin(), beg = begin(); beg != end(); ++inStart, ++beg)
+			{
+				if((int)(*inStart) == 255)
+				{
+					if ((int)(*beg) > 255) {
+						(int)(*beg) = 255;
+					}
+					else if ((int)(*beg) < 0) {
+						(int)(*beg) = 0;
+					}
+
+					int number = ((int)(*beg));
+					*inStart = (unsigned char)(number);
+				}
+			}
+
+			return tmp;
+		}
+
+		else {
+			exit(1);
+		}
+	}
+
+	Image Image::operator*(const int threshold) const
+	{
+		Image tmp(*this);
+		for(Image::iterator beg = begin(), inStart = tmp.begin(); beg != end(); ++beg, ++inStart)
+		{
+			if((int)(*inStart) <= threshold)
+			{
+				*inStart = (unsigned char)0;
+			}
+			else
+			{
+				*inStart = (unsigned char)255;
+			}
+		}
+
+		return tmp;
 	}
 
 
